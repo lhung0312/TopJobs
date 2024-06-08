d0351937a41183e1553a806b9338debbed493480
lhung0312 <hung0372899952@gmail.com>
2024-06-08 16:37:53 +0700

    update roleAPI

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,34 +1,54 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { ResponseMessage } from 'src/decorators/message.customize';
+import { IUser } from 'src/users/users.interface';
+import { User } from 'src/decorators/user.decorator';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
+  @ResponseMessage('create role')
   @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.rolesService.create(createRoleDto);
+  create(@Body() createRoleDto: CreateRoleDto, @User() user: IUser) {
+    return this.rolesService.create(createRoleDto, user);
   }
-
+  @ResponseMessage('Fetch role with paginate')
   @Get()
-  findAll() {
-    return this.rolesService.findAll();
+  findAll(
+    @Query('current') current: string,
+    @Query('pageSize') pageSize: string,
+    @Query() qs: string,
+  ) {
+    return this.rolesService.findAll(+current, +pageSize, qs);
   }
-
+  @ResponseMessage('Fetch role by id')
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
+    return this.rolesService.findOne(id);
   }
-
+  @ResponseMessage('delete role')
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateRoleDto: UpdateRoleDto,
+    @User() user: IUser,
+  ) {
+    return this.rolesService.update(id, updateRoleDto, user);
   }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.rolesService.remove(id, user);
   }
 }
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,26 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Role, RoleDocument } from './schemas/role.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import aqp from 'api-query-params';
+import mongoose from 'mongoose';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(
+    @InjectModel(Role.name)
+    private roleModel: SoftDeleteModel<RoleDocument>,
+    private configService: ConfigService,
+  ) {}
+  async create(createRoleDto: CreateRoleDto, user: IUser) {
+    const { name } = createRoleDto;
+    let isExist = await this.roleModel.findOne({ name });
+    if (isExist) {
+      throw new BadRequestException("Role's name existed");
+    }
+    let newRole = await this.roleModel.create({
+      ...createRoleDto,
+      createdBy: { _id: user._id, email: user.email },
+    });
+    return newRole;
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll(current: number, pageSize: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+    let offset = (current - 1) * pageSize;
+    let defaultPageSize = pageSize ? pageSize : 10;
+    const totalItems = (await this.roleModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultPageSize);
+    const result = await this.roleModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultPageSize)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+    return {
+      meta: {
+        current: current,
+        pageSize: pageSize,
+        pages: totalPages,
+        total: totalItems,
+      },
+      result,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: string) {
+    let role = await this.roleModel.findOne({ _id: id }).populate({
+      path: 'permissions',
+      select: { _id: 1, apiPath: 1, name: 1, method: 1, module: 1 },
+    });
+    return role;
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: string, updateRoleDto: UpdateRoleDto, user: IUser) {
+    return await this.roleModel.updateOne(
+      { _id: id },
+      {
+        ...updateRoleDto,
+        updatedBy: { _id: user._id, email: user.email },
+      },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: string, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('Invalid ID format');
+    }
+    const roleAdmin = this.configService.get<string>('ROLE_ADMIN');
+    const checkRole = await this.roleModel.findOne({ _id: id });
+    if (checkRole.name === roleAdmin) {
+      throw new BadRequestException('không thể xoá Role của Admin');
+    }
+    await this.roleModel.findByIdAndUpdate(
+      { _id: id },
+      {
+        deleteBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return await this.roleModel.softDelete({ _id: id });
   }
 }
