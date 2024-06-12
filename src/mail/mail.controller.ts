072cbd9ba1d2339bd67d3f74a2f519a89179a515
lhung0312 <hung0372899952@gmail.com>
2024-06-12 11:37:54 +0700

    create module mail & config server Email with Mailer

---
+++ src/mail/mail.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Public } from 'src/decorators/customizePublic';
+import { ResponseMessage } from 'src/decorators/message.customize';
+@Controller('mail')
+export class MailController {
+  constructor(
+    private readonly mailService: MailService,
+    private readonly mailerService: MailerService,
+  ) {}
+  @Get()
+  @Public()
+  @ResponseMessage('Test email')
+  async handleTestEmail() {
+    await this.mailerService.sendMail({
+      to: 'info.tuanhung@gmail.com',
+      from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to Nice App! Confirm your Email',
+      html: '<b>welcome bla bla</b>', // HTML body content
+    });
+  }
+}
